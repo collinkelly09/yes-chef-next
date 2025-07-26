728cd9e0b12362c99f0f3edf14e3f09165e93f6a
Collin Kelly <collinkelly09@gmail.com>
2025-07-25 20:10:13 -0600

    arrow functionality added

--- components/home/RecipesSlider.tsx
+++ components/home/RecipesSlider.tsx
@@ -7,15 +7,43 @@ import RecipeCard from "../recipes/RecipeCard";
 
 const RecipesSlider = ({ recipes }: { recipes: RecipeResponse[] }) => {
   const size = useWindowSize();
+
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider!.scrollLeft = slider!.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider!.scrollLeft = slider!.scrollLeft + 500;
+  };
+
   return (
-    <div className="flex items-center">
-      {size.width! > 768 && <ChevronLeft size={40} strokeWidth={1} />}
-      <div className="w-full overflow-x-scroll overflow-hidden no-scrollbar whitespace-nowrap scroll-smooth">
+    <div className=" relative flex items-center">
+      {size.width! > 768 && (
+        <ChevronLeft
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideLeft}
+          size={40}
+          strokeWidth={1}
+        />
+      )}
+      <div
+        id="slider"
+        className="w-full overflow-x-auto flex no-scrollbar whitespace-nowrap scroll-smooth"
+      >
         {recipes.map((recipe) => (
           <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </div>
-      {size.width! > 768 && <ChevronRight size={40} strokeWidth={1} />}
+      {size.width! > 768 && (
+        <ChevronRight
+          className="opacity-50 cursor-pointer hover:opacity-100"
+          onClick={slideRight}
+          size={40}
+          strokeWidth={1}
+        />
+      )}
     </div>
   );
 };
